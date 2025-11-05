2191f90113650cec0994c5c75747a2d4dd2e920f
renatakhabirva <renatahabirovka49@gmail.com>
2025-11-05 14:48:54 +0500

    Добавьте файлы проекта.

---
+++ Class.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace балахнин41
+{
+    internal class Class
+    {
+        
+    }
+    class Manager
+    {
+        public static Frame MainFrame { get; set; }
+    }
+}
